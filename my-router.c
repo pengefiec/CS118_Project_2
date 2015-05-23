494bde900e70b8c614b69365be27f61e65a0e5ec
artchar <artchar@gmail.com>
2015-05-23 07:29:35 -0700

    init file

---
+++ my-router.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <sys/types.h>   // definitions of a number of data types used in socket.h and netinet/in.h
+#include <sys/socket.h>  // definitions of structures needed for sockets, e.g. sockaddr
+#include <netinet/in.h>  // constants and structures needed for internet domain addresses, e.g. sockaddr_in
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/wait.h>	/* for the waitpid() system call */
+#include <signal.h>	/* signal name macros, and the kill() prototype */
