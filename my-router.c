ac1f4c68dad071031ff0c9e7f343865b53c645b0
artchar <artchar@gmail.com>
2015-05-23 09:45:35 -0700

    init file

---
+++ my-router.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <sys/types.h>   // definitions of a number of data types used in socket.h and netinet/in.h
+#include <sys/socket.h>  // definitions of structures needed for sockets, e.g. sockaddr
+#include <netinet/in.h>  // constants and structures needed for internet domain addresses, e.g. sockaddr_in
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/wait.h>	/* for the waitpid() system call */
+#include <signal.h>	/* signal name macros, and the kill() prototype */
+
+int start_socket(int port)
+{
+	int sockfd;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) 
+		error("ERROR opening socket");
+
+	struct sockaddr_in addr;
+	bzero((char *) &addr, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons(port);
+	if (bind(sockfd, (struct sockaddr *) &addr, sizeof(addr)) < 0)
+		error("error on binding");
+
+	return sockfd;
+
+}
+
+int main(int argc, char *argv[])
+{}
