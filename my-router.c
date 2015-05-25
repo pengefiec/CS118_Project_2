99a3069e3561eee20b737fa7d9b6b3f87896930b
artchar <arthr2@ucla.edu>
2015-05-24 17:55:18 -0700

    test

--- my-router.c
+++ my-router.c
@@ -9,9 +9,27 @@
 #include <sys/wait.h>	/* for the waitpid() system call */
 #include <signal.h>	/* signal name macros, and the kill() prototype */
 
-int start_socket(int port)
+const int ports[6] = {10001, 10000, 10002, 10003, 10004, 10005};
+
+void error(char *msg)
 {
+    perror(msg);
+    exit(1);
+}
+
+struct packet
+{
+	int source_port;
+	int dest_port;
+	char* msg;
+};
+
+
+int start_router(int port)
+{
+	signal(SIGPIPE, SIG_IGN);
 	int sockfd;
+	char buf[1024] = "hi";
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0) 
 		error("ERROR opening socket");
@@ -21,12 +39,56 @@ int start_socket(int port)
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = INADDR_ANY;
 	addr.sin_port = htons(port);
-	if (bind(sockfd, (struct sockaddr *) &addr, sizeof(addr)) < 0)
+	if (bind(sockfd, (struct sockaddr *) &addr, sizeof(struct sockaddr_in)) < 0)
 		error("error on binding");
 
-	return sockfd;
+
+	printf("Router waiting on port %d\n", port);
+
+	// Send message test
+	if (port == 10001)
+	{
+		struct packet p;
+		p.source_port = port;
+		p.dest_port = 10000;
+		p.msg = "hello test";
+
+		struct sockaddr_in remote_addr;
+		socklen_t remote_addr_len = sizeof(struct sockaddr_in);
+		remote_addr.sin_family = AF_INET;
+		remote_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+		remote_addr.sin_port = htons(10000);
+
+		if (sendto(sockfd, buf, sizeof(buf), 0, (struct sockaddr*)&remote_addr, sizeof(remote_addr)) < 0)
+			error("error in sending message");
+		else
+			printf("msg sent successfully");
+		return 0;
+	}
+
+
+	// Listening to receive message
+	while (true)
+	{
+		struct sockaddr_in remote_addr;
+		socklen_t remote_addr_len = sizeof(remote_addr);
+
+		struct packet* received_packet = (struct packet*)malloc(sizeof(struct packet));
+	
+		int recvlen = recvfrom(sockfd, received_packet, sizeof(struct packet), 0, (struct sockaddr *) &remote_addr, &remote_addr_len);
+		if (recvlen > 0)
+		{
+			buf[recvlen] = 0;
+			printf("msg received: %s\n", buf);
+		}
+	}
 
 }
 
 int main(int argc, char *argv[])
-{}
+{
+	int i;
+
+	// Create two routers for test
+	start_router(10001);
+}
